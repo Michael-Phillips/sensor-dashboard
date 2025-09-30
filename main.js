ee9059175d46bf15df57e0af5d2a291481f2eac4
Michael <michaeldphillips307@email.com>
2025-09-30 15:52:19 -0700

    no delete

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { getLatestPerDevice } from './utils.js';
 import { renderCards } from './renderCards.js';
-import { deleteCard } from './modal.js';
 
 const supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
