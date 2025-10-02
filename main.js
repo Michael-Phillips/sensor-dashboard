ae16c6c8d31f5e5563308388f8419db8513c2d95
Michael <michaeldphillips307@email.com>
2025-10-02 10:26:31 -0700

    clickable image

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import { getLatestPerDevice } from './utils.js';
 import { renderCards } from './renderCards.js';
 import { getCardSettings, createGearModal, closeModal } from './modal.js';
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://qvlluhoxehdpssdebzyi.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc'
+);
+
 
 const supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
