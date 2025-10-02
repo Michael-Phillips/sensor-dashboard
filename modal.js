f1262f0423aa5254d394bb275f068a92445b339d
Michael <michaeldphillips307@email.com>
2025-10-02 16:58:50 -0700

    again

--- modal.js
+++ modal.js
@@ -1,3 +1,4 @@
+//import { BASE_PATH } from './constants.js'; // Optional: if you centralize BASE_PATH
 import { getRelativeTime } from './utils.js';
 
 export function getCardSettings(cardId, data) {
