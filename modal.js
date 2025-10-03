85275a3ac4578da1838e50d7857e99d092c16e05
Michael <michaeldphillips307@email.com>
2025-10-03 14:48:55 -0700

    modal

--- modal.js
+++ modal.js
@@ -29,7 +29,7 @@ export function createGearModal(cardId, existingData, saveCardSettings, deleteCa
   console.log('📦 Modal content created');
 
   modalContent.innerHTML = `<h2>Modal for ${cardId}</h2>`;
-  <button id="closeModal">Close</button>
+  '<button id="closeModal">Close</button>';
   const descInput = document.createElement('input');
   descInput.type = 'text';
   descInput.placeholder = 'Description';
