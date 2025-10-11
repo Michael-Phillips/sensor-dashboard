64d55f2b3b4ed00bbb12e19e16b8c1b23bee0ede
Michael <michaeldphillips307@email.com>
2025-10-11 15:28:38 -0700

    preserve colors

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const supabase = window.supabase;
 
 const supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
+
+import { createClient } from '@supabase/supabase-js';
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
 const table = 'readings';
 
 let sensorData = [];
