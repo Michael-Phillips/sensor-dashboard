109eded4e7a14ac232c8a9fdb01a3abaedf81478
Michael-Phillips <Michael-Phillips@users.noreply.github.com>
2025-10-17 15:24:54 -0700

    new keys

--- supabase-init.js
+++ supabase-init.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 window.supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
 
 //window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
-window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
+window.supabaseKey = 'sb_publishable_avHO7oiQBQijHQ7jW_dYTg_AgNxSTiw';
 window.supabase = createClient(window.supabaseUrl, window.supabaseKey);
 
