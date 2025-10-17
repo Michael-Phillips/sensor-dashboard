6f7d80a53601cdaa192544727b3bc606f5a8a478
Michael-Phillips <Michael-Phillips@users.noreply.github.com>
2025-10-17 16:23:26 -0700

    Force GitHub Pages to refresh supabase-init.js

--- supabase-init.js
+++ supabase-init.js
@@ -2,10 +2,10 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 window.supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
 window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
-
+                     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzd…A5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc'
 window.supabase = createClient(window.supabaseUrl, window.supabaseKey);
 
-
+// force reload
 
 /*import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
