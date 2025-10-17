99f978ab6003bad17a105404283cff408d4a14e8
Michael-Phillips <Michael-Phillips@users.noreply.github.com>
2025-10-17 16:03:40 -0700

    new keys

--- supabase-init.js
+++ supabase-init.js
@@ -1,6 +1,15 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 window.supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
+window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
+
+window.supabase = createClient(window.supabaseUrl, window.supabaseKey);
+
+
+
+/*import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+window.supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
 window.supabaseKey = 'sb_publishable_avHO7oiQBQijHQ7jW_dYTg_AgNxSTiw';
 
 window.supabase = createClient(window.supabaseUrl, window.supabaseKey, {
@@ -10,6 +19,6 @@ window.supabase = createClient(window.supabaseUrl, window.supabaseKey, {
     },
   },
 });
-
+*/
 
 
