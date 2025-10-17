1b7eb435134f1b8e79f224597689ac6ef3af23c3
Michael-Phillips <Michael-Phillips@users.noreply.github.com>
2025-10-17 15:34:54 -0700

    new keys

--- supabase-init.js
+++ supabase-init.js
@@ -4,5 +4,13 @@ window.supabaseUrl = 'https://qvlluhoxehdpssdebzyi.supabase.co';
 
 //window.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGx1aG94ZWhkcHNzZGVienlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDMwOTQsImV4cCI6MjA3NDQxOTA5NH0.4sJas3fvz_2z5iPY6yqL8W2X0NgZYjKUxxGNJX-JAMc';
 window.supabaseKey = 'sb_publishable_avHO7oiQBQijHQ7jW_dYTg_AgNxSTiw';
-window.supabase = createClient(window.supabaseUrl, window.supabaseKey);
+//window.supabase = createClient(window.supabaseUrl, window.supabaseKey);
+window.supabase = supabase.createClient(window.supabaseUrl, window.supabaseKey, {
+  global: {
+    headers: {
+      Authorization: `Bearer ${window.supabaseKey}`,
+    },
+  },
+});
+
 
